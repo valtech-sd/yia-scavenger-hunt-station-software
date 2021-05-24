2f7666f0f64ddf33bd1a27c9814af4686a479b27
Eric A. Soto <Eric.Soto@valtech.com>
2021-05-24 19:41:09 -0400

    GIT LFS install being difficult

--- x-first-time-setup.sh
+++ x-first-time-setup.sh
@@ -6,7 +6,7 @@ sudo apt-get full-upgrade /y
 
 # Install GIT LFS
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt-get install git-lfs /y
+sudo apt-get install git-lfs -y
 git lfs install
 
 # Install NodeJS
