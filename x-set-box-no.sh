2583e6327135007d4932829f59b69ca360703c20
valtech-stash <stash.noga@valtech.com>
2021-05-27 09:34:08 -0400

    Set a global environment variable for BOX-ID as opposed to local user

--- x-set-box-no.sh
+++ x-set-box-no.sh
@@ -10,11 +10,11 @@ if [ $# -eq 0 ]
     exit 1
 fi
 
-echo "BOX_ID will be set to $1 (by adding to ~/.bashrc"
+echo "BOX_ID will be set to $1 (by adding to the /etc/environment file"
 # Wait for the user to press any KEY to proceed or allow them to Ctrl+C
 read -n1 -rsp $'Press any key to continue or Ctrl+C to exit...\n'
 
-echo "\n# Interactive Server Software" >> ~/.bashrc
-echo "export BOX_ID=$1" >> ~/.bashrc
+echo "\n# Interactive Server Software" >> /etc/environment
+echo "export BOX_ID=$1" >> /etc/environment
 
 echo "Be sure to start a new sell to see the new value."
