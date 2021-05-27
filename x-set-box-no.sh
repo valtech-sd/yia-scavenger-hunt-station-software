835a3c88e67b2f26710fef13a6a439cc25fe479c
valtech-stash <stash.noga@valtech.com>
2021-05-27 11:00:15 -0400

    remove new line from etc/environment append comment line

--- x-set-box-no.sh
+++ x-set-box-no.sh
@@ -14,7 +14,7 @@ echo "BOX_ID will be set to $1 (by adding to the /etc/environment file"
 # Wait for the user to press any KEY to proceed or allow them to Ctrl+C
 read -n1 -rsp $'Press any key to continue or Ctrl+C to exit...\n'
 
-echo "\n# Interactive Server Software" >> /etc/environment
+echo "# Interactive Server Software" >> /etc/environment
 echo "export BOX_ID=$1" >> /etc/environment
 
 echo "Be sure to start a new sell to see the new value."
